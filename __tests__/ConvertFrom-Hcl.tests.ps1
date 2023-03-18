c9222d9d71a70aee2b90fda4c5cacaa6b730eb4b
Mert Senel <32119844+MertSenel@users.noreply.github.com>
2023-03-18 15:59:23 +1100

    initiate module repo to test on CI (#1)
    * initiate module repo to test on CI
    
    * test CI again after updating file path with forward slashes
    
    * only run on windows to see if problem is OS specific
    
    * test filepath with logging commands
    
    * test with removed round brackets around filepath
    
    * try specifying full filepaths in test cases
    
    * enable tests in all OS platforms
    
    * define a default executable for linux
    
    * fix if else syntax for default executable for cross platform
    
    * only run tests on windows for now
    
    * add publish module workflow

---
+++ __tests__/ConvertFrom-Hcl.tests.ps1
@@ -0,0 +1,61 @@
+Import-Module "$PSScriptRoot/../Hcl2PS.psd1" -Force
+
+Describe "Test Conversions main.tf" -Tag 'main.tf File Tests' {
+
+    BeforeAll {
+        $filePath = Join-Path $PSScriptRoot "testData" main.tf
+        write-host "File Path: $filepath"
+        $actual = ConvertFrom-Hcl -Path $filePath 
+    }
+    It "main.tf should have blocks" {
+
+        ($actual | get-member -Type NoteProperty).name | Should -Be @("data", "provider", "resource", "terraform")
+    }
+
+    It "main.tf nested property value test" {
+
+        $actual.resource.azurerm_kubernetes_cluster.k8sexample.name | Should -Be '${var.vault_user}-k8sexample-cluster'
+    }
+}
+
+
+Describe "Test Conversions outputs.tf" -Tag 'outputs.tf File Tests' {
+
+    BeforeAll {
+        $filePath = Join-Path $PSScriptRoot "testData" outputs.tf
+        write-host "File Path: $filepath"
+        $actual = ConvertFrom-Hcl -Path $filePath
+    }
+    It "outputs.tf should have outputs" {
+
+        ($actual.output | get-member -Type NoteProperty).name | Should -Be @("environment", 
+                                                                            "k8s_endpoint", 
+                                                                            "k8s_id", 
+                                                                            "k8s_master_auth_client_certificate",
+                                                                            "k8s_master_auth_client_key", 
+                                                                            "k8s_master_auth_cluster_ca_certificate", 
+                                                                            "private_key_pem", 
+                                                                            "vault_addr",
+                                                                            "vault_user"
+                                                                            )
+    }
+
+    It "outputs.tf nested property value test" {
+
+        $actual.output.environment.value | Should -Be '${var.environment}'
+    }
+}
+
+Describe "Test Conversions sample-policy.hcl" -Tag 'sample-policy.hcl File Tests' {
+
+    BeforeAll {
+        $filePath = Join-Path $PSScriptRoot "testData" sample-policy.hcl
+        write-host "File Path: $filepath"
+        $actual = ConvertFrom-Hcl -Path $filePath
+    }   
+
+    It "sample-policy.hcl nested property value test" {
+
+        $actual.path.'auth/roger*'.capabilities | Should -Be @('create','read','update','delete','list')
+    }
+}
