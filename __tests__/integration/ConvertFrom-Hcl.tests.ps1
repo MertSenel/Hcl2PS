69247cd2f539be08a7f214cde550500b20a01d96
Mert Senel <mertsenel@gmail.com>
2023-08-20 11:29:13 +1000

    format test script file

--- __tests__/integration/ConvertFrom-Hcl.tests.ps1
+++ __tests__/integration/ConvertFrom-Hcl.tests.ps1
@@ -31,15 +31,15 @@ Describe "Test Conversions outputs.tf" -Tag 'outputs.tf File Tests' {
     It "outputs.tf should have outputs" {
 
         ($actual.output | get-member -Type NoteProperty).name | Should -Be @("environment", 
-                                                                            "k8s_endpoint", 
-                                                                            "k8s_id", 
-                                                                            "k8s_master_auth_client_certificate",
-                                                                            "k8s_master_auth_client_key", 
-                                                                            "k8s_master_auth_cluster_ca_certificate", 
-                                                                            "private_key_pem", 
-                                                                            "vault_addr",
-                                                                            "vault_user"
-                                                                            )
+            "k8s_endpoint", 
+            "k8s_id", 
+            "k8s_master_auth_client_certificate",
+            "k8s_master_auth_client_key", 
+            "k8s_master_auth_cluster_ca_certificate", 
+            "private_key_pem", 
+            "vault_addr",
+            "vault_user"
+        )
     }
 
     It "outputs.tf nested property value test" {
@@ -58,7 +58,7 @@ Describe "Test Conversions sample-policy.hcl" -Tag 'sample-policy.hcl File Tests
 
     It "sample-policy.hcl nested property value test" {
 
-        $actual.path.'auth/roger*'.capabilities | Should -Be @('create','read','update','delete','list')
+        $actual.path.'auth/roger*'.capabilities | Should -Be @('create', 'read', 'update', 'delete', 'list')
     }
 }
 
